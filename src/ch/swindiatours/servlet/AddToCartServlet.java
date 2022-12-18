abc62d0896fcb5d075d5860982805f86e1712c34
chantifa <chantifa@windowslive.com>
2022-12-18 20:20:07 +0100

    artefact corrected

--- src/ch/swindiatours/servlet/AddToCartServlet.java
+++ src/ch/swindiatours/servlet/AddToCartServlet.java
@@ -9,18 +9,19 @@ import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.Serial;
 import java.util.ArrayList;
 
 
 @WebServlet(name = "AddToCartServlet", value = "/add-to-cart")
 public class AddToCartServlet extends HttpServlet {
+    @Serial
     private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
 
         try (PrintWriter out = response.getWriter()) {
-//        	out.print("add to cart servlet");
 
             ArrayList<Cart> cartList = new ArrayList<>();
             int id = Integer.parseInt(request.getParameter("id"));
